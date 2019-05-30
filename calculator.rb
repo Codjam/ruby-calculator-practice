7cb414c65f5a943a76737de858017fe3726529b8
github username <github email address>
2019-05-30 14:30:48 -0400

    Now you can +, -, *, /, and %(modulo)

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,14 @@
 puts "Simple Calculator"
-25.times { print "-"}
+20.times { print "-"}
 puts
 puts "Enter the first number"
 num_1 = gets.chomp
 puts "Enter the second number"
 num_2 = gets.chomp
 puts "The first number multiplied by the second number is #{num_1.to_i * num_2.to_i}"
+puts "The first number divided by the second number is #{num_1.to_i / num_2.to_i}"
+puts "The first number subtracted by the second number is #{num_1.to_i - num_2.to_i}"
+puts "The first number added by the second number is #{num_1.to_i + num_2.to_i}"
+puts "The first number modulo by the second number is #{num_1.to_i % num_2.to_i}"
+
+#modulus % * / + - to make calculator fully functional
